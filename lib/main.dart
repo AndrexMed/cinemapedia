f50c6e4df582a02b41cc2b9f15f0658f3545f384
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-24 17:41:31 -0500

    Creacion del proyecto + estructura base P1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:cinemapedia/configs/router/app_router.dart';
+import 'package:cinemapedia/configs/theme/app_theme.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
