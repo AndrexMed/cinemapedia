5f39c4c1d4ee491ef26434131c944660a832c181
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-24 20:57:28 -0500

    Uso de Environments en la app

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:cinemapedia/configs/router/app_router.dart';
 import 'package:cinemapedia/configs/theme/app_theme.dart';
 
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MainApp());
 }
 
