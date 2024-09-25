1ae5018f71788aa9e48f7cda8c9b2a8fb30a5b39
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-25 11:20:13 -0500

    MovieRepository Implementation

---
+++ lib/infrastructure/repositories/movie_repository_implementation.dart
@@ -0,0 +1,12 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movie_repository.dart';
+
+class MovieRepositoryImplementation extends MoviesRepository {
+  final MoviesDatasource datasource;
+  MovieRepositoryImplementation({required this.datasource});
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+}
