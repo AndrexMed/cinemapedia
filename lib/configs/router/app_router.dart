e94857a45518a167e1d7ee6614716e2d17cca39f
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-27 17:29:40 -0500

    Este ajusto se realizo con el fin, de que si se accede directamente por url, no se pierde el boton back y bottomBar

--- lib/configs/router/app_router.dart
+++ lib/configs/router/app_router.dart
@@ -4,16 +4,17 @@ import '../../presentation/screens/screens.dart';
 
 final appRouter = GoRouter(initialLocation: '/', routes: [
   GoRoute(
-    path: '/',
-    name: HomeScreen.name,
-    builder: (context, state) => const HomeScreen(),
-  ),
-  GoRoute(
-    path: '/movie/:movieId',
-    name: MovieScreen.name,
-    builder: (context, state) {
-      final movieId = state.pathParameters['movieId'] ?? 'No Id';
-      return MovieScreen(movieId: movieId);
-    },
-  ),
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'movie/:movieId',
+          name: MovieScreen.name,
+          builder: (context, state) {
+            final movieId = state.pathParameters['movieId'] ?? 'No Id';
+            return MovieScreen(movieId: movieId);
+          },
+        ),
+      ]),
 ]);
