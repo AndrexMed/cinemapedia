bca575f708c7123a4f8f9d11a76387cc69f4531f
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-27 18:37:25 -0500

    MovieDetails Cache Local

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends StatefulWidget {
   static const name = 'movie_screen';
   final String movieId;
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  State<MovieScreen> createState() => _MovieScreenState();
+}
+
+class _MovieScreenState extends State<MovieScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Movie $movieId'),
+        title: Text('Movie ${widget.movieId}'),
       ),
     );
   }
--- lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,6 +1,5 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/configs/helpers/human_formats.dart';
-import 'package:cinemapedia/configs/router/app_router.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
