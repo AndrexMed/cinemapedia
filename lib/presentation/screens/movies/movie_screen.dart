b8145a45eed65471b139afb53eeecae578c83d71
GiovanniAndrex <giovanniandrex@gmail.com>
2024-09-27 19:24:14 -0500

    Disenio de la pantalla Movie Parte1

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -35,9 +35,102 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
       );
     }
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Movie ${widget.movieId}'),
+        body: CustomScrollView(
+      physics: const ClampingScrollPhysics(),
+      slivers: [
+        _MovieSliverAppBar(movie: movie),
+        SliverList(
+            delegate: SliverChildListDelegate([_MovieOverview(movie: movie)]))
+      ],
+    ));
+  }
+}
+
+class _MovieSliverAppBar extends StatelessWidget {
+  final Movie movie;
+  const _MovieSliverAppBar({required this.movie});
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+        backgroundColor: Colors.black,
+        expandedHeight: size.height * 0.7,
+        foregroundColor: Colors.white,
+        // floating: false,
+        // pinned: true,
+        flexibleSpace: FlexibleSpaceBar(
+            titlePadding:
+                const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            title: Text(
+              movie.title,
+              style: textTheme.titleMedium,
+              textAlign: TextAlign.start,
+            ),
+            background: Stack(
+              children: [
+                SizedBox.expand(
+                  child: Image.network(
+                    movie.posterPath,
+                    fit: BoxFit.cover,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress != null) {
+                        return const Center(
+                          child: CircularProgressIndicator(strokeWidth: 2),
+                        );
+                      }
+                      return child;
+                    },
+                  ),
+                ),
+
+                const SizedBox.expand(
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.black,
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
+                    ),
+                  ),
+                )
+                //const _BackdropAndTitle()
+              ],
+            )));
+  }
+}
+
+class _BackdropAndTitle extends StatelessWidget {
+  const _BackdropAndTitle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Positioned(
+      top: 0,
+      child: Container(
+        width: size.width,
+        height: size.height * 0.4,
+        color: Colors.red,
       ),
+    );
+  }
+}
+
+class _MovieOverview extends StatelessWidget {
+  final Movie movie;
+  const _MovieOverview({required this.movie});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Text(movie.overview),
     );
   }
 }
