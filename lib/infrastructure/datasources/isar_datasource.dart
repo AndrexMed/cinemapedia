6bf15fee4c0cc60157b8f34d75e6417dc5e0fcd9
GiovanniAndrex <giovanniandrex@gmail.com>
2024-10-02 13:58:04 -0500

    Repositorio y Datasources

---
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -0,0 +1,22 @@
+import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+class IsarDatasource extends LocalStorageDatasource {
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    // TODO: implement isMovieFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> loadFavoritesMovies({int limit = 10, int offset = 0}) {
+    // TODO: implement loadFavoritesMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
+    throw UnimplementedError();
+  }
+}
